54f55cbb05ae0bc92e0bd3d308f1c516f4088c37
Ivanna <ivannakucher0102@gmail.com>
2026-03-31 20:56:12 +0300

    feat: scaffold project layout with header, navbar, and footer components

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center bg-gray-800 text-white p-4">
+      <Navbar />
+      <Link href="/">
+        <h2 className="text-xl font-bold">FLASHNEWS</h2>
+      </Link>
+      <div>
+        <input
+          className="text-white px-4 py-2  border border-gray-700 rounded"
+          type="text"
+          placeholder="Search"
+        />
+        <button className="bg-gray-700 text-white px-4 py-2 rounded">
+          Search
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
