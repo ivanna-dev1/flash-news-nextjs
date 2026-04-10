bed11c5d777c67079590e1ba499caccf0143127f
Ivanna <ivannakucher0102@gmail.com>
2026-04-10 16:45:09 +0300

    feat: implement home page layout with news feed and category navigation

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         Stay informed with the latest news from around the world.
         <br /> We bring you accurate, timely, and relevant stories every day.
       </p>
-      <div className="flex flex-col gap-6 md:flex-row ">
+      <div className="flex flex-col gap-6  ">
         <div className="flex flex-col flex-2 gap-2">
           {news.map((article) => (
             <BigNewsCard article={article} key={article.id} />
