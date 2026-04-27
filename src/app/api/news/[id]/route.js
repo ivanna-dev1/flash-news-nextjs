642868fed64d620d87a4dcf01205e147111a7794
Ivanna <ivannakucher0102@gmail.com>
2026-04-27 17:53:41 +0300

    feat: implement news retrieval API endpoints for listing all articles and fetching by ID

--- src/app/api/news/[id]/route.js
+++ src/app/api/news/[id]/route.js
@@ -1,10 +1,13 @@
 import { NextResponse } from "next/server";
-import { news } from "../../arrayFakeNews.js";
+import { news } from "../arrayFakeNews.js";
 
-export async function GET() {
-  return (
-    <div>
-      <p>test</p>
-    </div>
-  );
+export async function GET(request, { params }) {
+  const { id } = await params;
+  const article = news.find((item) => item.id === Number(id));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  if (!article) {
+    return NextResponse.json({ error: "Article not found" }, { status: 404 });
+  }
+
+  return NextResponse.json(article);
 }
--- src/app/api/news/route.js
+++ src/app/api/news/route.js
@@ -2,6 +2,5 @@ import { NextResponse } from "next/server";
 import { news } from "./arrayFakeNews.js";
 
 export async function GET() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
   return NextResponse.json(news);
 }
